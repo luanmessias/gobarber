e3260f31b29f05adfbed67b0ee8817017bcc998f
luanmessias <luanmessias10@gmail.com>
2019-12-28 13:07:26 +0000

    Add provider section

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,10 @@ import { Router } from 'express';
 import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
-import FileController from './app/controllers/FileController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -15,6 +16,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
