62cec613dd8f1d529ecdce6a7d31ddc22fefb058
luanmessias <luanmessias10@gmail.com>
2019-12-28 16:17:33 +0000

    Add list appointments section

--- src/routes.js
+++ src/routes.js
@@ -22,7 +22,9 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
