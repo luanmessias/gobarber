b493fe066b2db6199b87069cad577f499394f7f8
luanmessias <luanmessias10@gmail.com>
2019-12-30 16:32:45 +0000

    Add method to send mail to provider when appointment is canceled

---
+++ src/config/mail.js
@@ -0,0 +1,26 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '94f6e0dd0a0db3',
+    pass: '436127f25afac3',
+  },
+  default: {
+    from: 'GoBarber Team <noreply@gobarber.com',
+  },
+};
+
+/**
+ * Quando estiver Online usar esses:
+ *
+ * Amazon SES
+ * Mailgun
+ * Sparkspot
+ * Mandril (Mailchimp)
+ * GMail
+ *
+ * Apenas para dev
+ * Mailtrap
+ *
+ */
