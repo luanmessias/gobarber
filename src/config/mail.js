1b0248dfc1732abe28e292c48dc799081f78869b
luanmessias <luanmessias10@gmail.com>
2020-01-09 10:59:31 +0000

    Add .env file and config ambient variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '94f6e0dd0a0db3',
-    pass: '436127f25afac3',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'GoBarber Team <noreply@gobarber.com',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://5bf4470466f3474fb32c2d5a8e4b45ec@sentry.io/1875867',
+  dsn: process.env.SENTRY_DSN,
 };
